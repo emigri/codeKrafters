c9cf85986e4118384391bb41e0bc4e76c105eb90
Steve Hannay <hannay.steve@googlemail.com>
2024-03-13 21:24:52 +0000

    new jsx files and some pseudo code

---
+++ src/components/pages/MeetOurTeam.jsx
@@ -0,0 +1,9 @@
+import React, { Component} from 'react';
+export default class MeetOurTeam extends Component  {
+    render() {
+        return (
+            // MeetOurTeam
+
+        )
+    }
+}
---
+++ src/components/pages/Slider.jsx
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+export default class Slider extends Component  {
+    render() {
+        return (
+            // Slider
+
+
+/* <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+<swiper-container>
+  <swiper-slide>Slide 1</swiper-slide>
+  <swiper-slide>Slide 2</swiper-slide>
+  <swiper-slide>Slide 3</swiper-slide>
+  <swiper-slide>Slide ...</swiper-slide>
+</swiper-container> */
+
+        )
+    }
+}
