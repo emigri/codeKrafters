db43f0d311bffeac303cc1cd1fa2c0de6905fe01
MMDPX <mmdpx@internet.ru>
2024-03-14 21:35:40 +0000

    Added the slider feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { Component} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import "./components/Products.json";
-import "./components/SiteContent.json";
+import Products from './components/Products.json';
+import SiteContent from './components/SiteContent.json';
+
+import Home from './components/pages/Home' 
+
+import '../src/App.css';
 
 export default class App extends Component {
   render() {
-    return <h1 className="text-3xl font-bold underline">Sample Text</h1>;
+    return (
+      <>
+      <h1 className="text-3xl font-bold underline">Sample Text</h1>
+      <Home></Home>
+      </>
+    )
   }
 }
