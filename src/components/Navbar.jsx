c02470ca9e80db36040da18cfb463883d09c10c7
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-03-16 14:32:57 +0000

    changes class into ClassName on html tags for footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,37 @@
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 // import "./styles/style.css";
 
+function Navbar() {
+  const navLinks = [
+    { title: "Home", path: "/" },
+    { title: "Product", path: "/product" },
+    { title: "About", path: "/about" },
+    { title: "Contact", path: "/contact" },
+    { title: "Basket", path: "/basket" }
+  ];
+  return (
+    <div className="w-full fixed">
+      <div className="md:px-10 py-4 px-7 flex justify-between items-center bg-blue-100">
+        <div className="flex text-2xl cursor-pointer items-center gap-1">
+          <span className="font-bold">codeKrafters</span>
+        </div>
 
-
- function Navbar() {
-   
-    const navLinks = [
-        {title: 'Home', path: '/'},
-        {title: 'Product', path: '/product'},
-        {title: 'About', path: '/about'},
-        {title: 'Contact', path: '/contact'},
-        {title: 'Basket', path: '/basket'},
-
-]
-        return (
-            <div className='w-full fixed'>
-                <div className='md:px-10 py-4 px-7 flex justify-between items-center bg-blue-100'>
-                  
-                    <div className='flex text-2xl cursor-pointer items-center gap-1'>
-                    <span className='font-bold'>codeKrafters</span>
-                    </div>
-                   
-                    <ul className='flex pl-5'>
-                    {navLinks.map((link, index) => (
-                                <li key={index}>
-                                    <Link className="font-semibold" to={link.path}>{link.title}</Link>
-                                </li>
-                            ))
-                        }
-                        <button onClick className='btn bg-sky-400 hover:bg-sky-700 text-white rounded '>Account</button>
-
-                    </ul>
-
-
-                </div>
-
-            </div>
-
-        );
-    }
+        <ul className="flex pl-5">
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <Link className="font-semibold" to={link.path}>
+                {link.title}
+              </Link>
+            </li>
+          ))}
+          <button className="btn bg-sky-400 hover:bg-sky-700 text-white rounded ">
+            Account
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default Navbar;
