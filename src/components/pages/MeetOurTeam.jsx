4088f90191ebe7a727ad6abbe66443d19e078c69
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-03-16 21:18:37 +0000

    styling to the meet our team

--- src/components/pages/MeetOurTeam.jsx
+++ src/components/pages/MeetOurTeam.jsx
@@ -27,7 +27,7 @@ function MeetOurTeam() {
                     alt=""
                   />
                   <div className="flex flex-col justify-between p-4 leading-normal">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <h5 key={developer} className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                       {developer}
                     </h5>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
