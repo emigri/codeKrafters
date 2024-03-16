c0e683ebb6bc05f9db8d006c2115ca1680190f6c
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-03-16 13:53:37 +0000

    added missing imports to App.jsx that were undefined

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import React, { Component} from 'react';
+import React, { Component } from "react";
 import Navbar from "./components/Navbar";
-import Home from './components/pages/Home';
-import Product from './components/pages/Product-Card';
-import Contact from './components/pages/Contact';
-import About from './components/pages/About';
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Products from './components/Products.json';
-import SiteContent from './components/SiteContent.json';
+import Home from "./components/pages/Home";
+import Product from "./components/pages/Product-Card";
+import ProductCatalogue from "./components/pages/Product-Catalogue";
+import Contact from "./components/pages/Contact";
+import About from "./components/pages/About";
+import Basket from "./components/pages/Basket";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Products from "./components/Products.json";
+import SiteContent from "./components/SiteContent.json";
 
-
- function App (){
- 
-    return (
-      <>
+function App() {
+  return (
+    <>
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Contact ' element={<Contact />} />
-          <Route path='/Basket ' element={<Basket />} />
-          <Route path='/ProductCatalogue ' element={<ProductCatalogue />} />
-
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact " element={<Contact />} />
+          <Route path="/Basket " element={<Basket />} />
+          <Route path="/ProductCatalogue " element={<ProductCatalogue />} />
         </Routes>
       </Router>
       <h1 className="text-3xl font-bold underline">Sample Text</h1>
-  
+
       <Home></Home>
-      </>
-    )
-  }
-  export default App;
+    </>
+  );
+}
+export default App;
