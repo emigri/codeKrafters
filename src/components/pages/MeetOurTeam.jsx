80751dc5b96dcb6dc2f0ae9d6e6cd0cb82b2d59a
Steve Hannay <hannay.steve@googlemail.com>
2024-03-21 17:29:29 +0000

    changes to content5

--- src/components/pages/MeetOurTeam.jsx
+++ src/components/pages/MeetOurTeam.jsx
@@ -9,7 +9,7 @@ function MeetOurTeam() {
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Meet our team!
           </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p className="mt-4 text-lg leading-8 text-gray-600">
             We are a team of five developers that work with HTML, CSS,
             Javascript and React
           </p>
