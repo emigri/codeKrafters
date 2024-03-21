f07569ed8de71d94e7249a4c3051f7c7cf5d42f7
Steve Hannay <hannay.steve@googlemail.com>
2024-03-21 17:11:50 +0000

    changes to content3

--- src/components/pages/Wrapper.jsx
+++ src/components/pages/Wrapper.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Wrapper(props) {
-  return <div className="wrapper container mx-auto">{props.children}</div>;
+  return (
+
+    <div>
+
+    
+      <div className="container my-10">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Our current promotions
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Select 'Our Products' from the meny to view all of our products
+          </p>
+      </div>
+    
+    <div className="wrapper container mx-auto">{props.children}</div>
+
+    </div>
+
+  )
 }
 
 export default Wrapper;
