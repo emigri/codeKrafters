1a1a30a769ef3fce67ccee7bc8f23ef2914d2940
Kazim Ozdil <skozdil@gmail.com>
2024-03-22 14:16:34 +0000

    contact css comoleted

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -13,30 +13,70 @@ function Contact() {
 
   return (
     <>
-    <br/>
-    <p>We are always happy to hear from our customers. Please complete the following:</p>
-    <br/>
-    <form id="contact-form" onSubmit={handleSubmit}>
-      <div className="form-group">
-        <p>Your name:</p>
-        <input type="text" className="input-field" id="name-input" value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <p>Your email:</p>
-        <input type="email" className="input-field" id="email-input" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div className="form-control">
-        <p>Your phone number:</p>
-        <input type="phone" className="input-field" id="phone-input" value={phone} onChange={(e) => setPhone(e.target.value)} />
-      </div>
-      <div className="form-control">
-        <p>Your message:</p>
-        <textarea className="input-field" rows="4" id="message-input" value={message} onChange={(e) => setMessage(e.target.value)} />
-      </div>
-      <button type="submit" className="c-btn">Submit</button>
-    </form>
-    <br/>
-    <h1>Alternatively, you can email us at support@codekrafters.co.uk or call us at +44 7444 555 666</h1>
+    <div className="w-full mx-auto contact">
+      <div>We are always happy to hear from our customers. Please complete the following:</div>
+      <form className="mx-auto max-w-sm">
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+              Your Name
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-800" id="inline-full-name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+              Your E-Mail
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-800" id="inline-full-email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+              Your Phone
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-800" id="inline-full-phone" type="text" value={phone} onChange={(e) => setPhone(e.target.value)} />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+              Your Message
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-800" id="inline-full-message" type="textarea" value={message} onChange={(e) => setMessage(e.target.value)}/>
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3"></div>
+          <label className="md:w-2/3 block text-gray-800 font-bold">
+            <input className="mr-2 leading-tight" type="checkbox"/>
+            <span className="text-sm">
+              Send me your newsletter!
+            </span>
+          </label>
+        </div>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <button className="shadow bg-teal-800 hover:bg-teal-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+      <div>
+      <h1>Alternatively, you can email us at support@codekrafters.co.uk or call us at +44 7444 555 666</h1></div>
+    </div>
   </>
   );
   
